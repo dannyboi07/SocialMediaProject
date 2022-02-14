4796ac6e4606828b476a4b8a478c780e6bee7e1c
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-02-15 01:04:49 +0530

    Delete src directory

--- src/services/loginService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3500/api/login";
-
-async function loginUser(userDetails) {
-  const response = await axios.post(baseUrl, userDetails);
-  return response.data;
-};
-
-export { loginUser };
--- src/services/registerService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3500/api/register";
-
-async function registerUser(userDetails) {
-  const response = await axios.post(baseUrl, userDetails, { headers: { "Content-Type": "multipart/form-data" }});
-  return response.data;
-};
-
-export { registerUser };
