8dfc43e3b91c16c24f59ce9976642d28277f6f0c
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-02-18 00:17:44 +0530

    Add files via upload

--- backend/controller/content.js
+++ backend/controller/content.js
@@ -3,10 +3,25 @@ const db = require("../db");
 const jwt = require("jsonwebtoken");
 
 contentRouter.get("/", async (req, res, next) => {
+
   const allPosts = await db.query("SELECT post.p_id, post.text, post.likes, post.date, users.u_id, users.name, users.username, users.imgloc FROM post JOIN users on users.u_id = post.user_id ORDER BY date desc");
+  
   const allBlogs = await db.query("SELECT blog.b_id, blog.text, blog.images, blog.likes, blog.date, users.u_id, users.name, users.username, users.imgloc FROM blog JOIN users on users.u_id = blog.user_id ORDER BY date desc");
+
+  // let postsLikes = [];
+  // for (let i = 0; i < allPosts.rows.length; i++) {
+  //   let temp;
+  //   db.query("SELECT COUNT(*) FROM likes_post_rel WHERE p_id_fk=$1", [allPosts.rows[i].p_id]).then(res => {
+  //     temp = res.rows[0].count;
+  //     console.log("count",res.rows[0].count);
+  //   });
+  //   console.log(temp)
+  //   // allPosts.rows[i].likes = temp;
+  //   // console.log("loop",allPosts.rows[i])
+  // }
+  
   const allContent = { blogs: [...allBlogs.rows], posts: [...allPosts.rows] };
-  console.log(allContent);
+  console.log("done");
   res.json(allContent);
 });
 
