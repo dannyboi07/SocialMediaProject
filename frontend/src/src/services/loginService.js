3d71d6b9d4347b9388b540adf24a7cc01deb5ab9
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-03-22 00:25:04 +0530

    Delete frontend/src/src directory

--- frontend/src/src/services/loginService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://192.168.42.206:3500/api/login";
-
-async function loginUser(userDetails) {
-  const response = await axios.post(baseUrl, userDetails);
-  return response.data;
-};
-
-export { loginUser };
